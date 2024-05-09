466e42bab645077f9ee8e53d854e40e918801ba6
dan <daniel_tzonkov@abv.bg>
2024-05-09 15:15:37 +0300

    09052024

---
+++ src/main/java/_04_RichEmployeesFirstName.java
@@ -0,0 +1,32 @@
+import entities.Town;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+import java.util.Scanner;
+
+public class _04_RichEmployeesFirstName {
+    public static void main(String[] args) {
+        EntityManagerFactory factory =
+                Persistence.createEntityManagerFactory("PU_Name");
+
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Scanner scanner = new Scanner(System.in);
+        String[] searchFor = scanner.nextLine().split(" ");
+
+        entityManager
+                .createQuery("SELECT COUNT(e) FROM Employee e WHERE e.firstName = :first_name AND e.lastName = :last_name",
+                        Long.class)
+                .setParameter("first_name", searchFor[0])
+                .setParameter("last_name", searchFor[1])
+                .getSingleResult();
+
+
+
+        entityManager.getTransaction().commit();
+    }
+}
