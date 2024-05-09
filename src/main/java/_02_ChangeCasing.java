fd77f002a9ca3686c28c270c102830fe0daa9d7b
dan <daniel_tzonkov@abv.bg>
2024-05-09 14:11:08 +0300

    09052024

--- src/main/java/_02_ChangeCasing.java
+++ src/main/java/_02_ChangeCasing.java
@@ -3,19 +3,22 @@ import entities.Town;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
 
-public class Main {
+public class _02_ChangeCasing {
     public static void main(String[] args) {
         EntityManagerFactory factory =
                 Persistence.createEntityManagerFactory("PU_Name");
 
         EntityManager entityManager = factory.createEntityManager();
-
         entityManager.getTransaction().begin();
 
-        Town town = entityManager.find(Town.class, 1);
-
-        System.out.println(town);
+        //Town town = entityManager.find(Town.class, 1);
+        //System.out.println(town);
+        Query from_town = entityManager.
+                createQuery("SELECT t FROM Town t", Town.class);
+        List<Town> resultList = from_town.getResultList();
 
         entityManager.getTransaction().commit();
     }
