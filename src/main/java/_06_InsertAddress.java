8e3cde68858dc8e4408a5becc51a7fe04e4d40c9
dan <daniel_tzonkov@abv.bg>
2024-05-10 08:54:48 +0300

    10052024

---
+++ src/main/java/_06_InsertAddress.java
@@ -0,0 +1,27 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class _06_InsertAddress {
+    public static void main(String[] args) {
+        EntityManagerFactory factory =
+                Persistence.createEntityManagerFactory("PU_Name");
+
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        List<String> resultList = entityManager
+                .createQuery("SELECT e.firstName FROM Employee e WHERE e.salary > 50000",
+                        String.class)
+                .getResultList();
+
+        String join = String.join("\n", resultList);
+
+        System.out.println(join);
+
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+    }
+}
