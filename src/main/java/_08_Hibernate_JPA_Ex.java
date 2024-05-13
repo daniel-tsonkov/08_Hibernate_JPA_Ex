139f18b913a64354fb8439900440f775611bc844
dan <daniel_tzonkov@abv.bg>
2024-05-13 14:02:20 +0300

    13052024

--- src/main/java/_08_Hibernate_JPA_Ex.java
+++ src/main/java/_08_Hibernate_JPA_Ex.java
@@ -1,5 +1,19 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class _08_Hibernate_JPA_Ex {
     public static void main(String[] args) {
+        EntityManagerFactory factory =
+                Persistence.createEntityManagerFactory("PU_Name");
 
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+
+
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
     }
 }
