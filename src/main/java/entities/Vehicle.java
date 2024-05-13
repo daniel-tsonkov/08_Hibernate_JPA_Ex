4e5acfa75861e8bcb06e2b5d6f21837eb7eda87b
dan <daniel_tzonkov@abv.bg>
2024-05-13 14:09:05 +0300

    13052024

--- src/main/java/entities/Vehicle.java
+++ src/main/java/entities/Vehicle.java
@@ -1,11 +1,22 @@
 package entities;
 
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private int id;
 
+    @Basic
     private String type;
 
     public Vehicle() {}
+
+    public Vehicle(String type) {
+        this.type = type;
+    }
 
     public int getId() {
         return this.id;
