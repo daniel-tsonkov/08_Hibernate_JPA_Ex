0959bc01cf74ed7f356f6a7c48a216478e800050
dan <daniel_tzonkov@abv.bg>
2024-05-13 14:04:58 +0300

    13052024

---
+++ src/main/java/entities/Vehicle.java
@@ -0,0 +1,25 @@
+package entities;
+
+public abstract class Vehicle {
+    private int id;
+
+    private String type;
+
+    public Vehicle() {}
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
